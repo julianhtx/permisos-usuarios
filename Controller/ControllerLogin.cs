bc71b7bccf270ba64cb648ce40d9a63087f2c46f
Julian Trejo <ljht.2004@gmail.com>
2024-10-07 08:10:08 -0600

    Avance del manejador del login

---
+++ Controller/ControllerLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Controller
+{
+    public class ControllerLogin
+    {
+        Funciones f = new Funciones();
+        public string[] Validar(string _nick, string _clave)
+        {
+            string[] resultado = new string[2];
+            DataSet r = f.Mostrar($"call validar('{_nick}', '{Sha1(_clave)}')", "usuarios");
+            DataTable dt = r.Tables[0];
+            resultado[0] = dt.Rows[0]["rs"].ToString();
+            resultado[1] = dt.Rows[0]["rol"].ToString();
+            return resultado;
+        }
+
+        public static string Sha1(string texto)
+        {
+            SHA1 sha1 = SHA1CryptoServiceProvider.Create();
+            Byte[] textOriginal = ASCIIEncoding.Default.GetBytes(texto);
+            Byte[] hash = sha1.ComputeHash(textOriginal);
+            StringBuilder cadena = new StringBuilder();
+            foreach (byte i in hash)
+            {
+                cadena.AppendFormat("{0:x2}", i);
+            }
+            return cadena.ToString();
+        }
+    }
+}
